3bfe24bdcd2f00195ca726f9ea9f1fdcff855cf6
Katherine Yu <katherine.l.yu@duke.edu>
2023-11-06 14:07:05 -0500

    ???

--- src/HashListAutocomplete.java
+++ src/HashListAutocomplete.java
@@ -36,14 +36,18 @@ public class HashListAutocomplete implements Autocompletor {
     public void initialize(String[] terms, double[] weights) {
         myMap = new HashMap<>();
         for (int i = 0; i < terms.length; i++) {
+
             String t = terms[i];
             int max = Math.min(t.length(), MAX_PREFIX);
             mySize +=BYTES_PER_CHAR*t.length();
+
             for (int j = 0; j <= max; j++) {
+
                 String prefix = t.substring(0, j);
                 Term newTerm = new Term(t, weights[i]);
-                mySize += BYTES_PER_CHAR*prefix.length() + BYTES_PER_DOUBLE;
-
+                if (!myMap.containsKey(prefix)) {
+                    mySize += BYTES_PER_CHAR*prefix.length() + BYTES_PER_DOUBLE;
+                }
                 myMap.putIfAbsent(prefix, new ArrayList<Term>());
                 myMap.get(prefix).add(newTerm);
             }
